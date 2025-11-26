d84ba845dfc2223a6fe152e6d291b4fe60a0d7ca
Thành KC <thanh.dao.gtsvietnam@gmail.com>
2025-11-26 22:59:14 +0700

    feat: add  locale and design system

---
+++ lib/shared/bloc/app_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_template/bloc/bloc_base.dart';
+
+class AppBloc extends BlocBase {
+  Ref ref;
+
+  AppBloc(this.ref);
+}
